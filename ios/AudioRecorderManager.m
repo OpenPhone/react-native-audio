ca9f3e0778e07fdcfb6ce353122839b1e8223dbf
Mahyar <mahyar@illusive.ca>
2017-09-26 18:41:42 -0400

    Added MP3

--- ios/AudioRecorderManager.m
+++ ios/AudioRecorderManager.m
@@ -138,6 +138,8 @@ RCT_EXPORT_METHOD(prepareRecordingAtPath:(NSString *)path sampleRate:(float)samp
       _audioEncoding =[NSNumber numberWithInt:kAudioFormatMPEGLayer1];
     } else if ([encoding  isEqual: @"mp2"]) {
       _audioEncoding =[NSNumber numberWithInt:kAudioFormatMPEGLayer2];
+    } else if ([encoding  isEqual: @"mp3"]) {
+        _audioEncoding =[NSNumber numberWithInt:kAudioFormatMPEGLayer3];
     } else if ([encoding  isEqual: @"alac"]) {
       _audioEncoding =[NSNumber numberWithInt:kAudioFormatAppleLossless];
     } else if ([encoding  isEqual: @"amr"]) {
